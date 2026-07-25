f323fe238425d62a169714605e13508b872a6c14
Saarthak50 <ysarthakyadav3737@gmail.com>
2026-07-25 16:11:09 +0530

    281A renammed

--- prob281A.java
+++ prob281A.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class prob281A {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         String word = sc.nextLine();
