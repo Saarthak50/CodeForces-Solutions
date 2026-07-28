542c5b8428b705c2aa383d533d1fbc5ce0f19ea6
Saarthak50 <ysarthakyadav3737@gmail.com>
2026-07-28 21:23:57 +0530

    155A

---
+++ AILoveUsername.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class AILoveUsername{
+    static int noOfAmazingPerformances(int n,int[] arr){
+        int max = arr[0];
+        int min= arr[0];
+        int count = 0;
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                count++;
+                max = arr[i];
+            }else if(arr[i]<min){
+                count++;
+                min = arr[i];
+            }
+            
+        }
+        return count;
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0 ; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(noOfAmazingPerformances(n, arr));
+        sc.close();
+
+       
+    }
+}
