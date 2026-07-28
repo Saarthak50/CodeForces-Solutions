a4c624d6bb9286f8f09b9c4f78ee6e9f0a69b2ef
Saarthak50 <ysarthakyadav3737@gmail.com>
2026-07-28 21:38:57 +0530

    1807A

---
+++ APlusOrMinus.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class APlusOrMinus{
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n-- > 0){
+            int a  = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if( a + b == c){
+                System.out.println('+');
+            }else{
+                System.out.println('-');
+            }
+        }
+
+    }
+}
