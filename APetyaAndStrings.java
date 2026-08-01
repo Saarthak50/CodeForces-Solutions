ef56ddf8f519dc352488a0bc4d91c556316471af
Saarthak50 <ysarthakyadav3737@gmail.com>
2026-08-01 23:12:57 +0530

    112A

---
+++ APetyaAndStrings.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class APetyaAndStrings{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next().toLowerCase();
+        String s2 = sc.next().toLowerCase();
+
+         int ans = s1.compareTo(s2);
+
+        if (ans < 0)
+            System.out.println(-1);
+        else if (ans > 0)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
