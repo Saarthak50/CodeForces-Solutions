93c87a4ecf0c869d05bd057bfcac20194f04cba7
Saarthak50 <ysarthakyadav3737@gmail.com>
2026-08-29 01:45:16 +0530

    339A

---
+++ AWordCapitalization.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AWordCapitalization{
+    public static void main(String[] args) {
+
+
+
+        Scanner sc =new Scanner(System.in);
+        String word = sc.nextLine();
+        String char1 = word.substring(0,1);
+        String rest = word.substring(1);
+        System.out.println(char1.toUpperCase()+rest);
+        sc.close();
+
+    
+
+
+    }
+
+}
